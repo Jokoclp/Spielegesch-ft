87baa0447c08c003ee452a04d1d4bfec1ee9474a
Jokoclp <jokohclp@gmail.com>
2020-05-12 12:40:08 +0200

    zrdz

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Header</title>
+
+</head>
+<body>
+<div class="footer">
+
+    <ul>
+        <li><h3 style="color:white;">Hilfe und Service</h3></li>
+        <li><a href="/impressum.php" >Impressum</a></li>
+        <li><a href="/impressum.php" >Datenschutz</a></li>
+
+    </ul>
+    <ul>
+        <li><h3 style="color:white;">Megagame</h3></li>
+        <li><a href="/impressum.php" >Über Uns</a></li>
+        <li><a href="/impressum.php" >Karriere</a></li>
+
+    </ul>
+    <ul>
+        <li><h3 style="color:white;">Öffnungszeiten</h3></li>
+        <p class="zeit">Mo-Fr: 08:00-17:00</p>
+        <p class="zeit">Sa: 08:00-17:00</p>
+        <p class="zeit">So: Geschlossen</p>
+    </ul>
+    <ul>
+        <li><h3 style="color:white;">Telefon:</h3></li>
+        <a href="tel:+490591123456">+490591/123456</a>
+    </ul>
+    <ul>
+        <img style="margin-left: 20px;margin-top: 45px;" src="img/logo_small.png" >
+    </ul>
+</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+    <meta charset="UTF-8">
+    <title>Gameshop</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="container"
+<?php include('header.php') ?>
+<?php include('footer.php') ?>
+
+
+    <P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    <P>test</P><P>test</P><P>test</P><P>test</P><P>test</P>
+    
+
+</div>
+</body>
+<script src="java.js"></script>
+</html>
